7209f9b733d0bace9195e07c691f0ed9709f1ca7
Yon <larin.andreika@yandex.ru>
2024-04-25 16:28:00 +0300

    add some variables

--- Proba/Program.cs
+++ Proba/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Хаюшки");
+﻿//Console.WriteLine("Хаюшки");
+int value = 100;
+Console.WriteLine(value);
+
+double d = 4.21;
+char c  = '!';  
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
