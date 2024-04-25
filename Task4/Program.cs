84f8323707aeb75b0ef47ab47a33e1552cebd0fd
Yon <larin.andreika@yandex.ru>
2024-04-25 16:54:49 +0300

    add lesson1 Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 55;
+int b = 4;
+int c = 1;
+int d = 7;
+int e = 14;
+
+int max = a;
+
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+if (d>max)
+{
+    max = d;
+}
+if (e>max)
+{
+    max = e;
+}
+Console.WriteLine(max);
