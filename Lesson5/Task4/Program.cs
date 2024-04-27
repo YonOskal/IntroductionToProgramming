51749d76f34064087af351f53d620415160a4b65
Yon <larin.andreika@yandex.ru>
2024-04-27 17:08:06 +0300

    Начала выполнять таск1 из дз

---
+++ Lesson5/Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Console.WriteLine("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if (max < min)
+// {
+//     int max = num2;
+//     int min = num1;
+// }
+// else if (max == num2);
+// {
+//     Console.WriteLine("Числа равны");
+// }
+// int min = num2
+// Console.WriteLine($"Максимальное число = {max}");
+// Console.WriteLine($"Минимальное число = {min}");
+// /////////////////
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+int min = num2;
+
+if (max < num2)
+{
+    max = num2;
+    min = num1;
+}
+else if (max == num2)
+{
+    Console.WriteLine("Числа равны");
+    // Возможно, здесь нужно что-то еще сделать в случае равенства чисел
+}
+Console.WriteLine($"Максимальное число = {max}");
+Console.WriteLine($"Минимальное число = {min}");
