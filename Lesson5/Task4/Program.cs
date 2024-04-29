f44f5ae387fb6d45c1638673a54be660ff58600c
Yon <larin.andreika@yandex.ru>
2024-04-29 21:00:21 +0300

    Закончили решение

--- Lesson5/Task4/Program.cs
+++ Lesson5/Task4/Program.cs
@@ -1,38 +1,10 @@
-﻿// Console.WriteLine("Введите первое число: ");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число: ");
-// int num2 = Convert.ToInt32(Console.ReadLine());
-// int max = num1;
-// if (max < min)
-// {
-//     int max = num2;
-//     int min = num1;
-// }
-// else if (max == num2);
-// {
-//     Console.WriteLine("Числа равны");
-// }
-// int min = num2
-// Console.WriteLine($"Максимальное число = {max}");
-// Console.WriteLine($"Минимальное число = {min}");
-// /////////////////
-Console.WriteLine("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+﻿int number = 5;
 
-int max = num1;
-int min = num2;
-
-if (max < num2)
+if (number % 2 == 0)
 {
-    max = num2;
-    min = num1;
+    Console.WriteLine(number + " четное");
 }
-else if (max == num2)
+else
 {
-    Console.WriteLine("Числа равны");
-    // Возможно, здесь нужно что-то еще сделать в случае равенства чисел
+    Console.WriteLine(number + " нечетное");
 }
-Console.WriteLine($"Максимальное число = {max}");
-Console.WriteLine($"Минимальное число = {min}");
