634b8f548249d10856839b8647bc16ce6066ce63
Yon <larin.andreika@yandex.ru>
2024-04-29 20:59:16 +0300

    Закончили решение

--- Lesson5/Task2/Program.cs
+++ Lesson5/Task2/Program.cs
@@ -4,15 +4,13 @@
 //4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
 //2 => -2, -1, 0, 1, 2
 
-using Internal;
-
 Console.WriteLine("Введите целое положительное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int count = -num; // num * -1
+int count = 0; // num * -1
 
 while (count <= num)
 {
 Console.Write(count + " ");
-count++; // count = count + 1; //count += 1; сtrl+c остановить программу
+count= count + 2; // count = count + 1; //count += 1; сtrl+c остановить программу
 }
