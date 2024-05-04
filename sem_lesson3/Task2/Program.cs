95d560233d21a8b4ec738a6d90046d9289bc2acc
Yon <larin.andreika@yandex.ru>
2024-05-05 00:28:02 +0300

    add task2 sem3

---
+++ sem_lesson3/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { -1, -3, 8, -4, 3, -8, 3, 5, -2, 9 };
+
+int Length = array.Length;
+
+for (int i = 0; i < Length; i++)
+{
+    if (array[i] < 0)
+    {
+        array[i] = -array[i];
+    }
+    else if (array[i] > 0)
+    {
+        array[i] = -array[i];
+    }
+Console.Write(array[i] + " ");
+}
