03b9017d7dd2b82983abadd597cf18c4e2a76b41
Yon <larin.andreika@yandex.ru>
2024-05-18 18:45:11 +0300

    закончили работу

---
+++ sem_lesson1/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 7;
+int b = 5;
+int c = 9;
+int max = a;
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    Console.WriteLine(b);
+}
