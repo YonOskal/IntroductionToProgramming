352e5945ff4a8373f9cdae7f9431f19fb2c82cfc
Yon <larin.andreika@yandex.ru>
2024-05-18 18:44:27 +0300

    закончили работу

---
+++ sem_lesson2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int SecondDigit = 0;
+int ThirdDigit = 0;
+if (num >= 100 && num <= 999)
+{
+    SecondDigit = num / 10 % 10;
+    ThirdDigit = num % 10;
+}
+else
+{
+    Console.WriteLine("Некоректный ввод");
+}
+int result = SecondDigit;
+int count = 1;
+while (count < ThirdDigit)
+{
+    result *= SecondDigit; //result = result * SecondDigit
+    count++;
+}
+ Console.WriteLine(result);
--- sem_lesson2/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ sem_lesson2/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c81a1db44fc67fa812a13aee216edd65be8f4efd")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
