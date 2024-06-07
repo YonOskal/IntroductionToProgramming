f126ccd7401fded8f0b14b6e2f6489792e4bd820
Yon <larin.andreika@yandex.ru>
2024-06-07 19:05:09 +0300

    add Task1 sem6

--- sem_lesson6/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ sem_lesson6/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece49df7f288be6907de2d6f51c00e7d445b3ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19534399af9ade02a7f81cbc968f1d473fb0e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
