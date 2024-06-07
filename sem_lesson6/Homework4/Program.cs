e9c2418d474cb7d9335f58caf5fb2cb15fcb5532
Yon <larin.andreika@yandex.ru>
2024-06-07 19:04:18 +0300

    add sem6 homework4

---
+++ sem_lesson6/Homework4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+
+static string ReverseWord(string strNew)
+{
+    string[] strNewSplit = strNew.Split(); // ["Hello", "my", "world"]
+
+    string res = "";
+
+    for (int i = strNewSplit.Length - 1; i >= 0; i--)
+    {
+        res += strNewSplit[i] + " ";
+    }
+
+    return res;
+}
+
+string strNew = "Hello my world";
+string res = ReverseWord(strNew);
+Console.WriteLine(res);
