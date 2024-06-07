a19534399af9ade02a7f81cbc968f1d473fb0e90
Yon <larin.andreika@yandex.ru>
2024-06-07 18:39:36 +0300

    add sem6 task3

---
+++ sem_lesson6/Homework3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+static bool IsPalindrome(string inp)
+{
+    for (int i = 0; i < inp.Length / 2; i++)
+    {
+        if (inp[i] != inp[inp.Length - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+Console.WriteLine("Напишите строку: ");
+string input = Console.ReadLine();
+
+if (IsPalindrome(input))
+{
+    Console.WriteLine($"Слово \"{input}\" является палиндромом.");
+}
+else
+{
+    Console.WriteLine($"Слово \"{input}\" не является палиндромом.");
+}
+    
