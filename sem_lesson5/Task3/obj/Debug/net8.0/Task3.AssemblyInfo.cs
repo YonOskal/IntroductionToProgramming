92f5ad84e29d947afd825406c4b5f0c18d5c2b46
Yon <larin.andreika@yandex.ru>
2024-06-23 18:59:45 +0300

    add sem4,5

--- sem_lesson5/Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ sem_lesson5/Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c0aa682a8b4ff72b0557097dbc3988bf932fa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42dad840745044bb6a615d23139cb98638248d22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
